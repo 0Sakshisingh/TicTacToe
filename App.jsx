0d8f889613180c864ee3f9d461b4468565a32d3e
0Sakshisingh <88961782+0Sakshisingh@users.noreply.github.com>
2022-05-26 14:35:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Board from './component/Board';
+import { calculateWinner } from './helpers.jsx';
+
+import './style/style.scss';
+
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const message = winner ? `Winner is ${winner}`: `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
+
+export default App;
+
