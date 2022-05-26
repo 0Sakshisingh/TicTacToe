631c7626c794c6be35396977a19d294905bc3aef
0Sakshisingh <88961782+0Sakshisingh@users.noreply.github.com>
2022-05-26 14:44:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
+import Board from './component/Board';
+import { calculateWinner } from './helpers.jsx';
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+import './style/style.scss';
+
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const message = winner ? `Winner is ${winner}`: `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
+
+export default App;
